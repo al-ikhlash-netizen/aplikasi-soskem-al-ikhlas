bbb4d8adc5b18b1c1ba91d00a5430c8b08c898e9
MASJID JAMI AL IKHLASH <alikhlashmasjidjami@gmail.com>
2026-09-17 19:42:07 +0700

    Cache only successful HTTP responses in service worker

--- sw.js
+++ sw.js
@@ -12,22 +12,20 @@ self.addEventListener('install',event=>{
 self.addEventListener('activate',event=>{
   event.waitUntil(
     caches.keys().then(keys=>Promise.all(
-      keys
-        .filter(k=>k.startsWith('soskem-al-ikhlash-') && k!==CACHE_NAME)
-        .map(k=>caches.delete(k))
+      keys.filter(key=>key.startsWith('soskem-al-ikhlash-')&&key!==CACHE_NAME).map(key=>caches.delete(key))
     )).then(()=>self.clients.claim())
   );
 });
 
 self.addEventListener('fetch',event=>{
-  if(event.request.method!=='GET' || new URL(event.request.url).origin!==self.location.origin) return;
+  if(event.request.method!=='GET'||new URL(event.request.url).origin!==self.location.origin)return;
   event.respondWith(
-    fetch(event.request)
-      .then(response=>{
+    fetch(event.request).then(response=>{
+      if(response.ok){
         const copy=response.clone();
         caches.open(CACHE_NAME).then(cache=>cache.put(event.request,copy));
-        return response;
-      })
-      .catch(()=>caches.match(event.request).then(resp=>resp || caches.match('./index.html')))
+      }
+      return response;
+    }).catch(()=>caches.match(event.request).then(response=>response||(event.request.mode==='navigate'?caches.match('./index.html'):new Response('',{status:504}))))
   );
 });
